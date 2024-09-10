981c4814b989c7a430c73be4d7c5f9f505e33998
AnhTuan0810 <140954312+AnhTuan0810@users.noreply.github.com>
2024-09-10 15:48:14 +0700

    Revert "Dang ki va dang nhap"
    This reverts commit 5719590af9fd5bb8fe578172148997ba8cf87475.

--- DecorGearDomain/Data/Entities/User.cs
+++ DecorGearDomain/Data/Entities/User.cs
@@ -14,23 +14,19 @@ namespace DecorGearDomain.Data.Entities
     public class User : EntityBase
     {
         [Required]
-        public string ?UserID { get; set; }  
+        public string UserID { get; set; }  
 
         public string Name { get; set; }
 
         public string PhoneNumber { get; set; }
 
         [EmailAddress]
-        [Required(ErrorMessage = "Email is required")]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "Username is required")]
-        [RegularExpression(@"^[a-zA-Z0-9_]{5,20}$", ErrorMessage = "Username must be between 5 and 20 characters and contain only letters, numbers, and underscores.")]
+        [Required]
         public string UserName { get; set; }
 
-        [Required(ErrorMessage = "Password is required")]
-        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters long.")]
-        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{6,}$", ErrorMessage = "Password must be at least 6 characters long and contain at least one uppercase letter, one lowercase letter, one digit, and one special character.")]
+        [Required]
         public string Password { get; set; }
 
         [Required]
