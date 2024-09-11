9ec5ec57dc768b12a7f3e643bab7390c7100d243
HUNTERDEAD2004 <149474145+HUNTERDEAD2004@users.noreply.github.com>
2024-09-12 06:13:27 +0700

    interface

---
+++ DecorGearApplication/DataTransferObj/Sale/UpdateSaleRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecorGearApplication.DataTransferObj.Sale
+{
+    internal class UpdateSaleRequest
+    {
+        public int SaleID { get; set; }
+
+        public string SaleName { get; set; }
+
+        public int SalePercent { get; set; }
+
+        public string UserID { get; set; }
+    }
+}
---
+++ DecorGearApplication/Interface/IProductRespository.cs
@@ -0,0 +1,20 @@
+﻿using DecorGearApplication.DataTransferObj.Order;
+using DecorGearApplication.DataTransferObj.Product;
+using DecorGearDomain.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecorGearApplication.Interface
+{
+    internal interface IProductRespository
+    {
+        Task<List<ProductDto>> GetAllProduct(CancellationToken cancellationToken);
+        Task<ProductDto> GetKeyProductById(ViewProductRequest request, CancellationToken cancellationToken);
+        Task<ErrorMessage> CreateProduct(CreateProductRequest request, CancellationToken cancellationToken);
+        Task<ErrorMessage> UpdateProduct(UpdateProductRequest request, CancellationToken cancellationToken);
+        Task<bool> DeleteProduct(DeleteProductRequest request, CancellationToken cancellationToken);
+    }
+}
